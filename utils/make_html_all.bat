30cf8c1c0d0c07709c2582fe092a193773bc4cc6
robbod <robbod>
2001-11-21 17:14:04 +0000

    no message

--- utils/make_html_all.bat
+++ utils/make_html_all.bat
@@ -1,5 +1,5 @@
 @echo off
-REM $Id: make_html_all.bat,v 1.9 2001-09-12 09:16:31+01 rob Exp rob $
+REM $Id: make_html_all.bat,v 1.1 2001/11/21 15:41:31 robbod Exp $
 
 REM Generate the html for all the modules.
 
@@ -8,72 +8,73 @@ REM +++++++++++++++++++++++++++++++++++++++
 REM create the html for all the modules
 REM ++ copy the next line for each new module
 
-cscript express2xml.js advanced_boundary_representation arm
-cscript express2xml.js alias_identification arm
-cscript express2xml.js approval arm
-cscript express2xml.js boundary_representation_model arm
-cscript express2xml.js certification arm
-cscript express2xml.js configuration_effectivity arm
-cscript express2xml.js constructive_solid_geometry arm
-cscript express2xml.js constructive_solid_geometry_2d arm
-cscript express2xml.js constructive_solid_geometry_3d arm
-cscript express2xml.js contract arm
-cscript express2xml.js date_time arm
-cscript express2xml.js date_time_assignment arm
-cscript express2xml.js document_and_version_identification arm
-cscript express2xml.js document_assignment arm
-cscript express2xml.js document_definition arm
-cscript express2xml.js document_properties arm
-cscript express2xml.js document_structure arm
-cscript express2xml.js edge_based_wireframe arm
-cscript express2xml.js effectivity arm
-cscript express2xml.js effectivity_application arm
-cscript express2xml.js elemental_geometric_shape arm
-cscript express2xml.js elemental_topology arm
-cscript express2xml.js end_item_identification arm
-cscript express2xml.js event arm
-cscript express2xml.js external_item_identification_assignment arm
-cscript express2xml.js faceted_boundary_representation arm
-cscript express2xml.js faceted_boundary_representation_model arm
-cscript express2xml.js file_identification arm
-cscript express2xml.js file_properties arm
-cscript express2xml.js foundation_representation arm
-cscript express2xml.js geometric_shape_with_topology arm
-cscript express2xml.js geometric_validation_property_representation arm
-cscript express2xml.js geometrically_bounded_surface arm
-cscript express2xml.js geometrically_bounded_wireframe arm
-cscript express2xml.js identification_assignment arm
-cscript express2xml.js independent_property arm
-cscript express2xml.js independent_property_representation arm
-cscript express2xml.js independent_property_usage arm
-cscript express2xml.js manifold_surface arm
-cscript express2xml.js part_and_version_identification arm
-cscript express2xml.js part_occurrence arm
-cscript express2xml.js part_structure arm
-cscript express2xml.js part_view_definition arm
-cscript express2xml.js person_organisation arm
-cscript express2xml.js person_organisation_assignment arm
-cscript express2xml.js process_property_assignment arm
-cscript express2xml.js product_categorisation arm
-cscript express2xml.js product_concept_identification arm
-cscript express2xml.js product_identification arm
-cscript express2xml.js product_structure arm
-cscript express2xml.js product_version arm
-cscript express2xml.js product_version_structure arm
-cscript express2xml.js product_view_definition arm
-cscript express2xml.js product_view_definition_properties arm
-cscript express2xml.js product_view_definition_structure arm
-cscript express2xml.js product_view_definition_structure_properties arm
-cscript express2xml.js project arm
-cscript express2xml.js property_assignment arm
-cscript express2xml.js property_representation arm
-cscript express2xml.js security_classification arm
-cscript express2xml.js shape_property_assignment arm
-cscript express2xml.js shape_property_representation arm
-cscript express2xml.js shell_based_wireframe arm
-cscript express2xml.js solid_model arm
-cscript express2xml.js time_interval arm
-cscript express2xml.js topologically_bounded_surface arm
-cscript express2xml.js work_order arm
-cscript express2xml.js work_request arm
+call make_html advanced_boundary_representation
+call make_html alias_identification
+call make_html approval
+call make_html boundary_representation_model
+call make_html certification
+call make_html configuration_effectivity
+call make_html constructive_solid_geometry
+call make_html constructive_solid_geometry_2d
+call make_html constructive_solid_geometry_3d
+call make_html contract
+call make_html date_time
+call make_html date_time_assignment
+call make_html document_and_version_identification
+call make_html document_assignment
+call make_html document_definition
+call make_html document_properties
+call make_html document_structure
+call make_html edge_based_wireframe
+call make_html effectivity
+call make_html effectivity_application
+call make_html elemental_geometric_shape
+call make_html elemental_topology
+call make_html end_item_identification
+call make_html event
+call make_html external_item_identification_assignment
+call make_html faceted_boundary_representation
+call make_html faceted_boundary_representation_model
+call make_html file_identification
+call make_html file_properties
+call make_html foundation_representation
+call make_html geometric_shape_with_topology
+call make_html geometric_validation_property_representation
+call make_html geometrically_bounded_surface
+call make_html geometrically_bounded_wireframe
+call make_html identification_assignment
+call make_html independent_property
+call make_html independent_property_representation
+call make_html independent_property_usage
+call make_html manifold_surface
+call make_html part_and_version_identification
+call make_html part_occurrence
+call make_html part_structure
+call make_html part_view_definition
+call make_html person_organisation
+call make_html person_organisation_assignment
+call make_html process_property_assignment
+call make_html product_categorisation
+call make_html product_concept_identification
+call make_html product_identification
+call make_html product_structure
+call make_html product_version
+call make_html product_version_structure
+call make_html product_view_definition
+call make_html product_view_definition_properties
+call make_html product_view_definition_structure
+call make_html product_view_definition_structure_properties
+call make_html project
+call make_html property_assignment
+call make_html property_representation
+call make_html security_classification
+call make_html shape_property_assignment
+call make_html shape_property_representation
+call make_html shell_based_wireframe
+call make_html solid_model
+call make_html time_interval
+call make_html topologically_bounded_surface
+call make_html work_order
+call make_html work_request
 
+:END

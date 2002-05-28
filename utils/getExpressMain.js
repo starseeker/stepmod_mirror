449ed7eb3e853da6a7cb4744574d149d51312554
robbod <robbod>
2002-05-28 05:42:17 +0000

    Now takes a specified list of modules

--- utils/getExpressMain.js
+++ utils/getExpressMain.js
@@ -1,4 +1,4 @@
-//$Id: imagemap2xml.js,v 1.2 2002/01/29 17:23:38 robbod Exp $
+//$Id: getExpressMain.js,v 1.1 2002/03/11 10:15:40 robbod Exp $
 //  Author: Rob Bodington, Eurostep Limited
 //  Owner:  Developed by Eurostep 
 //  Purpose:  JScript to copy all the express files from the repository to
@@ -21,6 +21,8 @@
 //
 //------------------------------------------------------------
 
+// If 1 then output user messages
+var outputUsermessage = 1;
 
 
 function Debug(txt) {
@@ -34,7 +36,8 @@ function UserMessage(msg){
 
 function ErrorMessage(msg){
     var objShell = WScript.CreateObject("WScript.Shell");
-    WScript.Echo(msg);
+    if (outputUsermessage == 1)
+	WScript.Echo(msg);
     objShell.Popup(msg);
 }
 
@@ -72,10 +75,11 @@ function GetIrExpress(expDir) {
 
 
 // Copy across the MIM and the ARM express
-function GetArmMimExpress(expDir) {
+function GetArmMimExpressOld(expDir) {
     var fso = new ActiveXObject("Scripting.FileSystemObject");
     var armFldr = fso.GetFolder(expDir+"/arm");
     var mimFldr = fso.GetFolder(expDir+"/mim");
+
     var index = "../repository_index.xml";
     var xml = new ActiveXObject("Msxml2.DOMDocument.3.0");
     xml.async = false;
@@ -83,9 +87,52 @@ function GetArmMimExpress(expDir) {
     var expr = "/repository_index/modules/module";
     var moduleNodes = xml.selectNodes(expr);
     var members = moduleNodes.length;
+    UserMessage("Making:"+members);
     for (var i = 0; i < members; i++) {	
 	var node = moduleNodes(i);
 	var moduleName = node.attributes.getNamedItem("name").nodeValue;
+	var src, dst;
+
+	// Copy across mim file
+	var mimFileName = "../data/modules/"+moduleName+"/mim.exp";
+	var newMimFileName = expDir+"/mim/"+moduleName+"_mim.exp";
+	//UserMessage(mimFileName+"->"+newMimFileName);
+
+	if (fso.FileExists(mimFileName)) {
+	    src = fso.GetFile(mimFileName);
+	    dst = newMimFileName;
+	    src.Copy(dst);
+	} else {
+	    UserMessage("File does not exist:\n"+mimFileName);
+	}
+	    
+	// Copy across arm file
+	var armFileName = "../data/modules/"+moduleName+"/arm.exp";
+	var newArmFileName = expDir+"/arm/"+moduleName+"_arm.exp";
+	if (fso.FileExists(armFileName)) {
+	    src = fso.GetFile(armFileName);
+	    dst = newArmFileName;
+	    src.Copy(dst);
+	} else {
+	    UserMessage("File does not exist:\n"+armFileName);
+	}
+    }
+}
+
+function GetArmMimExpress(expDir, modList) {
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+    var armFldr = fso.GetFolder(expDir+"/arm");
+    var mimFldr = fso.GetFolder(expDir+"/mim");
+
+    if (!fso.FileExists(modList)) {
+	ErrorMessage("The "+modList+" file does not exist");
+	return(0);
+    }
+
+    var modListStr = fso.OpenTextFile(modList,1);
+
+    while (!modListStr.AtEndOfStream) {
+	var moduleName =  modListStr.ReadLine();
 	var src, dst;
 
 	// Copy across mim file
@@ -120,6 +167,7 @@ function MakeDirs(expDir) {
     if (fso.FolderExists(expDir)) {
 	ErrorMessage("Directory exists:\n"+expDir);
 	return(-1);
+
     } else {
 	fso.CreateFolder(expDir);
 	fso.CreateFolder(expDir+"/arm");
@@ -129,6 +177,118 @@ function MakeDirs(expDir) {
     return(retVal);
 }
 
+function AppendFiles(src, dst) {
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+    var ForAppending = 8;
+    var ForReading = 1;
+    var srcStr = fso.OpenTextFile(src,ForReading);
+    var dstStr = fso.OpenTextFile(dst,ForAppending);
+
+    while (!srcStr.AtEndOfStream) {
+	var l =  srcStr.ReadLine();
+	dstStr.WriteLine(l);
+    }
+    srcStr.Close();
+    dstStr.Close();
+}
+
+// Append all the express files
+function AppendMimArm(armOrMim, expDir, modList) {
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+    var ForAppending = 8;
+    var ForReading = 1;
+    var schemaArr = new Array;
+
+    var armFile = expDir+"/"+armOrMim+"/"+armOrMim+".exp";    
+    // make sure that the arm.exp file does not exist
+    if (fso.FileExists(armFile)) {
+	ErrorMessage("The "+armOrMim+".exp already exists. Delete to proceed\n"+armFile);
+	return(0);
+    }  
+
+    if (!fso.FileExists(modList)) {
+	ErrorMessage("The "+modList+" file does not exist");
+	return(0);
+    }  
+    var modListStr = fso.OpenTextFile(modList,ForReading);
+    var armStr = fso.CreateTextFile(armFile, true);
+
+    var srcPath = fso.GetFolder(expDir).Path.toString();
+
+    var reg = /{.*iso\b/;
+    while (!modListStr.AtEndOfStream) {
+	var src =  modListStr.ReadLine();
+	src = srcPath+"\\"+armOrMim+"\\"+src+"_"+armOrMim+".exp";
+	if (fso.FileExists(src)) {
+	    var srcStr = fso.OpenTextFile(src,ForReading);	
+	    armStr.WriteLine("");
+	    armStr.WriteLine("");
+	    armStr.WriteLine("(*");
+	    armStr.WriteLine("   ------------------------------------------------------------");
+	    armStr.WriteLine(src);
+	    armStr.WriteLine("   ------------------------------------------------------------");
+	    armStr.WriteLine("*)");
+	    armStr.WriteLine("");
+	    while (!srcStr.AtEndOfStream) {
+		var l =  srcStr.ReadLine();
+		// ignore 
+		// {iso standard 10303 part (11) version (4)} 
+		if (!l.match(reg))
+		    armStr.WriteLine(l);
+	    }
+	    srcStr.Close();    
+	}
+    }
+    armStr.Close();
+}
+
+function AppendMimArmOld(armOrMim, expDir, modList) {
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+    var ForAppending = 8;
+    var ForReading = 1;
+    var schemaArr = new Array;
+
+    var armFile = expDir+"/"+armOrMim+"/"+armOrMim+".exp";    
+    // make sure that the arm.exp file does not exist
+    if (fso.FileExists(armFile)) {
+	ErrorMessage("The "+armOrMim+".exp already exists. Delete to proceed\n"+armFile);
+	return(0);
+    }
+
+    var armFldr = fso.GetFolder(expDir+"/"+armOrMim);
+    var fc = new Enumerator(armFldr.files);
+    for (; !fc.atEnd(); fc.moveNext()) {
+	    var fpath = fc.item();
+	    if (fpath.type == "EXP File") 
+		schemaArr.push(fpath);
+    }
+
+    var armStr = fso.CreateTextFile(armFile, true);
+
+    var reg = /{.*iso\b/;
+
+    for (var i=0; i<schemaArr.length; i++) {	
+	var src = schemaArr[i];
+	var srcStr = fso.OpenTextFile(src,ForReading);
+	armStr.WriteLine("");
+	armStr.WriteLine("");
+	armStr.WriteLine("(*");
+	armStr.WriteLine("   ------------------------------------------------------------");
+	armStr.WriteLine(src);
+	armStr.WriteLine("   ------------------------------------------------------------");
+	armStr.WriteLine("*)");
+	armStr.WriteLine("");
+	while (!srcStr.AtEndOfStream) {
+	    var l =  srcStr.ReadLine();
+	    // ignore 
+	    // {iso standard 10303 part (11) version (4)} 
+	    if (!l.match(reg))
+		armStr.WriteLine(l);
+	}
+	srcStr.Close();    
+    }
+    armStr.Close();
+}
 
 
 function TestArgs() {
@@ -142,6 +302,28 @@ function TestArgs() {
 	return(1);
     }
 }
+
+function TestParams(expDir, modList) {
+    var armFile = expDir+"/arm/arm.exp";    
+    var mimFile = expDir+"/mim/mim.exp";    
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+
+    if (fso.FolderExists(expDir)) {
+	ErrorMessage("Directory exists:\n"+expDir);
+	return(-1);
+    } else if (!fso.FileExists(modList)) {
+	ErrorMessage("The "+modList+" file does not exist");
+	return(-1);
+    } else if (fso.FileExists(armFile)) {
+	ErrorMessage("The "+armFile+" already exists. Delete to proceed\n"+armFile);
+	return(-1);
+    } else  if (fso.FileExists(mimFile)) {
+	ErrorMessage("The "+mimFile+" already exists. Delete to proceed\n"+armFile);
+	return(-1);
+    }  
+    return(1);
+}
+
 
 
 // Given the name of a new directory, <dir> create a three sub directories
@@ -164,12 +346,16 @@ function Main() {
 }
 
 //Run from getEpxress.wsf
-function MainWindow(expDir) {
-    if (MakeDirs(expDir) == 1) {
-	GetArmMimExpress(expDir);
+function MainWindow(expDir, modList) {
+    if (TestParams(expDir, modList) == 1) {
+	MakeDirs(expDir);
+	GetArmMimExpress(expDir, modList);
 	GetIrExpress(expDir);
+	AppendMimArm("arm",expDir,modList);
+	AppendMimArm("mim",expDir,modList);
 	UserMessage("Created directory:\n  "+expDir);
     }
 }
 
 
+//MainWindow("..\\express", "..\\modlist");

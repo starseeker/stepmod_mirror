0c706f241f408e9e9bc962bbca05d1c3d7577c1b
radack <radack>
2007-04-20 22:01:30 +0000

    Used variable SAXON_HOME rather than hardcoded path.

--- utils/mkcmrecord/mkcmrecord.bat
+++ utils/mkcmrecord/mkcmrecord.bat
@@ -1,3 +1,3 @@
-set saxon=c:/software/saxonb8-8j
+set saxon=%SAXON_HOME%
 set classpath=.;mkcmrecord.jar;%saxon%/saxon8.jar;%saxon%/saxon8-dom.jar
 java -classpath "%classpath%" MakeCMRecordGui ../.. %1

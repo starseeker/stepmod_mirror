dfec93cd81702679449cf752654652b176b7b60f
radack <radack>
2007-06-26 23:20:14 +0000

    Added log4j logging.

--- utils/mkcmrecord/mkcmrecord.bat
+++ utils/mkcmrecord/mkcmrecord.bat
@@ -1,3 +1,3 @@
 set saxon=%SAXON_HOME%
-set classpath=.;mkcmrecord.jar;%saxon%/saxon8.jar;%saxon%/saxon8-dom.jar
+set classpath=.;mkcmrecord.jar;%saxon%/saxon8.jar;%saxon%/saxon8-dom.jar;c:/software/logging-log4j-1.2.14/dist/lib/log4j-1.2.14.jar
 java -classpath "%classpath%" MakeCMRecordGui ../.. %1

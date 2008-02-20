170885c138bd15312810bb65464a0e79a568649b
radack <radack>
2008-02-20 02:44:38 +0000

    Added constructor with exception argument.

--- utils/mkcmrecord/java/InternalErrorException.java
+++ utils/mkcmrecord/java/InternalErrorException.java
@@ -2,4 +2,7 @@ public class InternalErrorException extends Exception {
     public InternalErrorException(String msg) {
 	super(msg);
     }
+    public InternalErrorException(Exception e) {
+	super(e);
+    }
 }

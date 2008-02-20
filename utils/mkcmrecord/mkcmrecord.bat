a12b2096955bbe1feacbe5ba206422c6e1e407b6
radack <radack>
2008-02-20 02:25:56 +0000

    Self-contained.

--- utils/mkcmrecord/mkcmrecord.bat
+++ utils/mkcmrecord/mkcmrecord.bat
@@ -1,3 +1,1 @@
-set classpath=.;mkcmrecord.jar;lib/saxon8.jar;lib/saxon8-dom.jar;lib/log4j-1.2.14.jar;lib/org-netbeans-lib-cvsclient.jar
-set cvsroot=:pserver:anonymous@stepmod.cvs.sourceforge.net:/cvsroot/stepmod
-java -Dcvs.root=%cvsroot% -classpath "%classpath%" MakeCMRecordGui ../.. %1
+java -classpath .;dist/MakeCmRecord.jar MakeCMRecordGui

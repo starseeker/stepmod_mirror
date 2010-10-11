bc72bbfa80181298b64fb0033d457fc496759388
radack <radack>
2010-10-11 00:27:11 +0000

    Use strict. Enhanced checking.

--- utils/part1000/Desig_info.pm
+++ utils/part1000/Desig_info.pm
@@ -1,21 +1,37 @@
 package Desig_info;
 
+use strict;
 use Date_info;
+use Utils;
+
+my $date_pattern = "[0-9]{4}(:?[0-9]{2})?";
 
 sub new {
-    my ($class, $designation) = @_;
+    my ($class, $designator) = @_;
     my $self;
 
-    $designation = $designation.trim;
+    $designator = Utils::trim($designator);
     $self = {};
-    if ($designation =~ m|(ISO)(?:/(TS))? +10303-([0-9]+):([0-9]+(?:-[0-9]+)?)(?:\((E)\))|) {
-	$self->{_designation} = $designation;
-	$self->{_organization} = $1;
-	$self->{_deliverable_type} = $2;
-	$self->{_part_number} = int($3);
-	$self->{_date_str} = $4;
-	$self->{_date_info} = new Date_info($4);
-	$self->{_language} = $5;
+    if ($designator =~ m|(ISO)(?:/(TS))? ([-0-9]+)(?:[:]([-0-9]*))?(?:\((E)\))?|) {
+	my $organization = $1;
+	my $deliverable_type = $2;
+	my $standard_part_str = $3;
+	my $date_str = $4;
+	my $language= $5;
+	$self->{_designator} = $designator;
+	$self->{_organization} = $organization;
+	$self->{_deliverable_type} = $deliverable_type;
+	if ($standard_part_str =~ m|([0-9]*)(?:[-]([0-9]*))|) {
+	    my $standard_number_str = $1;
+	    my $part_number_str = $2;
+	    $self->{_standard_number} = int($standard_number_str);
+	    $self->{_part_number} = int($part_number_str);
+	}
+	$self->{_date_str} = $date_str;
+	if ($date_str) {
+	    $self->{_date_info} = new Date_info($date_str);
+	}
+	$self->{_language} = $7;
     }
     else {
 	print "Did not match 1.\n";
@@ -27,11 +43,15 @@ sub new {
 sub print {
     my ($self) = @_;
 
+    print "designator = $self->{_designator}\n";
     print "organization = $self->{_organization}\n";
     print "deliverable_type = $self->{_deliverable_type}\n";
+    print "standard_number = $self->{_standard_number}\n";
     print "part_number = $self->{_part_number}\n";
     print "date_str = $self->{_date_str}\n";
-    $self->{_date_info}->print();
+    if ($self->{_date_info}) {
+	$self->{_date_info}->print();
+    }
     print "language = $self->{_language}\n";
 }
 

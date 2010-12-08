1d917bbe56db204434aa1985572c646f6c4a436c
lothartklein <lothartklein>
2010-12-08 23:50:31 +0000

    roll back

---
+++ etc/ap210/tools/getmetadata/run.bat
@@ -0,0 +1,2 @@
+set base=c:\Program Files\Apache Software Foundation\Tomcat 5.5\webapps
+java -classpath . GetMetadata "%base%\AP210" report.txt errors.html metadata.txt
---
+++ etc/ap210/tools/lksoft/longform_gen/longform_arm.bat
@@ -0,0 +1,1 @@
+@java -cp longform.jar modules.LongForm longform_arm.exp longform_arm.prop >out 2>outerr

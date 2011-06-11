26525543db8e70f73457276c8aa4c3bc49d69e4d
radack <radack>
2011-06-11 21:30:17 +0000

    Replaced target-dir with input-dir and output-dir.

--- utils/part1000/overlay_parts.pl
+++ utils/part1000/overlay_parts.pl
@@ -22,7 +22,9 @@
 #
 #   module-list=<module_list> path of the file containing the list of modules
 #   resource-list=<resource_list> path of the file containing the list of resource parts
-#   target-dir=<AP_dir> path of the directory containing the AP
+#   input-dir  = <AP_dir> path of the directory containing the AP
+#   output-dir = <AP_dir> path of the directory to which the AP with overlaid
+#                parts is to be written
 #
 # Environment variables:
 #
@@ -42,8 +44,6 @@ use File::Copy;
 use File::Path qw(rmtree);
 use Getopt::Long;
 
-my $dir_mode = 0;
-
 BEGIN {
     $| = 1
 }
@@ -60,104 +60,55 @@ sub main {
 
     my $module_list_path = "";
     my $resource_list_path = "";
-    my $target_dir_path = "";
+    my $input_dir_path = "";
+    my $output_dir_path = "";
     my $verbose;
     my $by_subdirs;
 
+    print "hello\n";
     my $result = GetOptions ("module-list=s" => \$module_list_path,    # string
 			     "resource-list=s" => \$resource_list_path,# string
-			     "target-dir=s" => \$target_dir_path,    # string
-			     "by-subdirs" => \$by_subdirs, # flag
-			     "verbose" => \$verbose);  # flag
-    if ($by_subdirs) {
-	$dir_mode = 1;
-	process_by_subdirs("$part1000_home/data/resources", "$target_dir_path/data/resources");
-	process_by_subdirs("$part1000_home/data/modules", "$target_dir_path/data/modules");
-    }
-    else {
-	my @module_list = read_part_list($module_list_path);
-	my @resource_list = read_part_list($resource_list_path);
-	process_by_list("$part1000_home/data/modules", "$target_dir_path/data/modules", @module_list);
-	process_by_list("$part1000_home/data/resources", "$target_dir_path/data/resources", @resource_list);
-    }
+			     "input-dir=s" => \$input_dir_path,        # string
+			     "output-dir=s" => \$output_dir_path,      # string
+			     "by-subdirs" => \$by_subdirs,             # flag
+			     "verbose" => \$verbose);                  # flag
+    process_node($input_dir_path, $output_dir_path, "");
 }
 
-sub read_part_list {
-    my ($filename) = @_;
-    my @part_list;
-    open LISTFILE, $filename or die "Cannot read part list $filename: $!";
-    while (<LISTFILE>) {
-	my $part = $_;
-	$part =~ s/^\s+//;
-	$part =~ s/--.*$//;
-	$part =~ s/_arm;//;
-	$part =~ s/\s+$//;
-	$part =~ tr/[A-Z]/[a-z]/;
-	push(@part_list, $part);
-    }
-    close LISTFILE;
-    return @part_list;
-}
-
-sub process_by_subdirs {
-    my ($source_group_path, $target_group_path) = @_;
-    opendir my($dh), $target_group_path or die "Cannot open directory ($target_group_path): $!";
-    my @parts = grep { /^[a-zA-Z]/ } readdir($dh);
-    closedir $dh;
-
-    foreach my $part_name (@parts) {
-	print "processing part: $part_name\n";
-	process_part("$source_group_path/$part_name", "$target_group_path/$part_name");
-    }
-}
-
-sub delete_children {
-    my ($dir_path) = @_;
-    opendir my($dh), $dir_path or die "Cannot open directory ($dir_path): $!";
-    my @children = grep { /^[^.]/ } readdir($dh);
-    closedir $dh;
-
-    print "deleting children of $dir_path\n";
-    foreach my $child (@children) {
-	rmtree("$dir_path/$child") or die "Cannot delete tree ($dir_path/$child): $!";
-    }
-}
-
-sub process_by_list {
-    my ($source_group_path, $target_group_path, @parts) = @_;
-
-    delete_children($target_group_path);
-
-    foreach my $part_name (@parts) {
-	print "processing part: $part_name\n";
-	process_part("$source_group_path/$part_name", "$target_group_path/$part_name");
-    }
-}
-
-sub process_part {
-    my ($source_part_path, $target_part_path) = @_;
-    rmtree($target_part_path);
-    copy_dir($source_part_path, $target_part_path);
-}
-
-sub copy_dir {
-    my ($source_path, $target_path) = @_;
-
-    opendir my($dh), $source_path or die "Cannot open directory ($source_path): $!";
-    my @children = grep { /^[^.]/ } readdir($dh);
-    closedir $dh;
-
-    $target_path =~ s^/^\\^g;
-    mkdir($target_path) or die "Cannot make directory ($target_path): $!\n";
-
-    foreach my $child (@children) {
-	my $source_child = "$source_path/$child";
-	my $target_child = "$target_path/$child";
-	if (-d $source_child) {
-	    copy_dir("$source_child", "$target_child");
+sub process_node {
+    my ($source_root, $dest_root, $node_rel_path) = @_;
+    print "node_rel_path=$node_rel_path\n";
+    my $source_node_path = "$source_root/$node_rel_path";
+    print "source_node_path=$source_node_path\n";
+    my $dest_node_path = "$dest_root/$node_rel_path";
+    print "dest_node_path=$dest_node_path\n";
+    my $part1000_node_path = "$part1000_home/$node_rel_path";
+    if (-d $source_node_path) {
+	unless (-d $dest_node_path) {
+	    mkdir($dest_node_path, "777") or die "Could not create directory";
 	}
-	elsif (-f $source_child) {
-	    copy($source_child, $target_child);
+	opendir my($dh), $source_node_path or die "cannot open dir $source_node_path: $!";
+	my @children = grep { /^[^\.]/ } readdir($dh);
+	closedir $dh;
+
+	my $child;
+	my $basename = basename($node_rel_path);
+	print "basename=$basename\n";
+	if ($basename eq "modules" || $basename eq "resource_parts" || $basename eq "resources") {
+	    print "case 1\n";
+	    foreach $child (@children) {
+		dircopy($part1000_node_path, $dest_node_path) or warn "cannot copy directory $part1000_node_path: $!";
+	    }
+	}
+	else {
+	    print "case 2\n";
+	    foreach $child (@children) {
+		process_node($source_root, $dest_root, "$node_rel_path/$child");
+	    }
 	}
     }
+    if (-f $source_node_path) {
+	File::Copy($source_node_path, $dest_node_path);
+    }
 }
+

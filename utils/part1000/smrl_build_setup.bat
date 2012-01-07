ef3b2a47f8f8133a53ed06ecc58b0fdf0770f099
thomasrthurman <thomasrthurman>
2012-01-07 17:17:13 +0000

    changed in preparation for v5

--- utils/part1000/smrl_build_setup.bat
+++ utils/part1000/smrl_build_setup.bat
@@ -1,6 +1,5 @@
-Rem $Id: $
-Rem filename: smrl_build_setup.bat
-Rem in data/part1000/dvlp
+Rem $Id: smrl_build_setup.bat,v 1.5 2012/01/07 17:15:26 thomasrthurman Exp $
+Rem Copy to data/part1000/dvlp in local repo for execution ease
 set JAVA_HOME=C:\Program Files\Java\jdk1.6.0
 set ANT_HOME=C:\apache-ant\apache-ant-1.8.0RC1
 set SAXON_HOME=C:\saxon9\saxonb9-1-0-8j

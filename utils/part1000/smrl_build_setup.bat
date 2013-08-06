9521717ef892c05e862e86409ceec8ad139bf0a1
thomasrthurman <thomasrthurman>
2013-08-06 23:51:13 +0000

    updates for later version of tools

--- utils/part1000/smrl_build_setup.bat
+++ utils/part1000/smrl_build_setup.bat
@@ -1,14 +1,16 @@
-Rem $Id: smrl_build_setup.bat,v 1.5 2012/01/07 17:15:26 thomasrthurman Exp $
-Rem Copy to data/part1000/dvlp in local repo for execution ease
-set JAVA_HOME=C:\Program Files\Java\jdk1.6.0
-set ANT_HOME=C:\apache-ant\apache-ant-1.8.0RC1
+Rem $Id: smrl_build_setup.bat,v 1.6 2012/01/07 17:17:13 thomasrthurman Exp $
+Rem file stepmod/utils/part1000/smrl_build_setup.bat
+Rem Copy to data/library/dvlp in local repo for execution ease
+REm Java/<version>/lib/tools.jar is required. get it from jdk
+set JAVA_HOME=C:\Program Files\Java\jre7
+set ANT_HOME=C:\apache-ant\apache-ant-1.9.2
 set SAXON_HOME=C:\saxon9\saxonb9-1-0-8j
 set PERL_HOME=C:\strawberry\perl
 set WINDOZE_PATH=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem
 echo
 echo
-set PATH=%WINDOZE_PATH%;C:\strawberry\c\bin;C:\strawberry\perl\bin;%ANT_HOME%\bin;%JAVA_HOME%\bin
+set PATH=%WINDOZE_PATH%;C:\strawberry\c\bin;C:\strawberry\perl\bin;%ANT_HOME%\bin;%JAVA_HOME%\bin;%SAXON_HOME%
 echo %PATH%
 set TERM=dumb
-set SMRL_ROOT=U:\2012\SMRL_v5
+set SMRL_ROOT=U:\Documents\2013\SMRL_v5_rc2
 set WORK=U:\Downloads\tmp

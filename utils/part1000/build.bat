8a3ef1452f4614236fe02a485a9030d1ab33a25a
thomasrthurman <thomasrthurman>
2013-12-03 22:20:09 +0000

    updates to SMRL build.

--- utils/part1000/build.bat
+++ utils/part1000/build.bat
@@ -4,19 +4,21 @@ rem Usage:
 rem   build <target>
 rem
 rem See build.xml for the targets.
+rem smrl_build_setup.bat must be executed to create variables
 
 if not defined SMRL_ROOT (
 echo Environment variable SMRL_ROOT not defined
 echo Exiting
 exit /b 1
 )
+rem users\Gerry\cm-work\part1000\trunk
 
 if not defined WORK (
 echo Environment variable WORK not defined
 echo Exiting
 exit /b 1
 )
-
+rem 
 if not exist %SMRL_ROOT% (
 echo Directory %SMRL_ROOT% does not exist
 echo Exiting
@@ -29,4 +31,5 @@ echo Exiting
 exit /b 1
 )
 
+echo buiding...
 ant %1

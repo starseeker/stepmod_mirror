aba3e561a7258a053ba12c43b8183b065d9044a0
thomasrthurman <thomasrthurman>
2015-04-04 21:18:13 +0000

    added EDMS scripts, need to be tailored for local directory path to EDMS executable and to eclipse stepmod root.

---
+++ data/modules/design_product_data_management/dvlp/express/longform_gen/longform_edms.bat
@@ -0,0 +1,6 @@
+rem batch file for edms
+rem C:\edm\edm5.00.0302.07\bin\Edms longform.EDMScript
+rem C:\edm\edm5.01.0100.b04\bin\Edms longform.EDMScript
+C:\edm\edm5.01.0100.103\bin\edms longform.EDMScript
+rem C:\edm\edm5.02.0100.09-win32-vs6-128mod\bin\edms longform.EDMScript
+

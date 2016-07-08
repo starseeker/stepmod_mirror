f964b787bb93761f1c02a8d94e7aef5ffa5d6080
kevletu <kevletu>
2016-07-08 15:35:34 +0000

    second version from April 2016, documented

--- utils/update_CR_module_metadata.pl
+++ utils/update_CR_module_metadata.pl
@@ -1,34 +1,27 @@
-################################################################
-# Launches the update from a folder in Kevin's computer
-# /Users/klt/Projects/EclipseWS/stepmod/etc/ap242/
-# For input excel sheet with a named columns table:
-# A = 'WG12 nb', B,C,D empty, E = no name and contains 'ISO 10303-', F  ='Module part number', 
-# G = no name and contains 'ed', H = 'Edition', I ='Module name', J = 'Element type'
-# K = no name, contains the date, L='wg updated in stepmod?'
-
-
-
-
-
 #!/usr/bin/perl -w
+
+##### The file needs to be put in /stepmod/utils/
+##### To launch the script:
+#####	0) For input: A = N number, B = name, C and D = nothing, E = name of the document, F = date
+#####	1) make sure that perl is installed on your computer
+#####	2) make sure that perl modules (Spreadsheet::ParseXLSX, XML::Twig, and File::Copy are installed)
+#####	3) using your command line tool, set your working directory to stepmod/utils
+#####	4) launch the script 'perl update_module.pl'
+
 use strict;
 use warnings;
 use Spreadsheet::ParseXLSX;
 use XML::Twig;
 use File::Copy qw(move);
 
-###############################################################
-# main
-###############################################################
 
 ###############################################################
-# Open an excel file
+#Open the excel file
 ###############################################################
 
 my $parser= Spreadsheet::ParseXLSX->new();
-# Check the true location in STEPmod
-#my $workbook= $parser->parse("/Users/aminata/Documents/ISO/WG12/STEP-ISO10303/stepmod/AP242ed2_CR11_WG_Numbers.xlsx");
-my $workbook= $parser->parse("/Users/klt/Projects/EclipseWS/stepmod/etc/ap242/AP242ed2_CR11_WG_Numbers.xlsx");
+#my $workbook= $parser->parse("/Users/aminatambengue/Documents/ISO/WG12/STEP-ISO10303/stepmod/CR_Geometry_WG_Numbers_table_example.xlsx");
+my $workbook= $parser->parse("../publication/part1000/CR_Geometry/CR_Geometry_WG_Numbers_table.xlsx");
 
 if (!defined $workbook) {
 	die $parser->error(), ".\n";
@@ -37,324 +30,185 @@ if (!defined $workbook) {
 ###############################################################
 # Collecting the different values in the excel file 
 ###############################################################
-#Creation of a table composed of the necessary values for the update 
+# Columns number in the excel sheet
+my $WGnb_col = 0;
+my $info_col = 4;
+# initialization of the variables
+my @type;
 my @table;
-my @doctable;
-#Column number in the excel sheet:
-my $WGnb_col;
-my $ed_col;
-my $type_col;
-my $modified_col;
-my $name_col;
-# Line number of the module:
-my $ligne;
-my $count = 1;
+# Read the excel sheet and save the variables 
 for my $worksheet ($workbook->worksheets()) {
-	my ($row_min, $row_max) = $worksheet->row_range();
-	my ($col_min, $col_max) = $worksheet->col_range();
-
-	#Search and save of the different columns
-	my $rowi = 0;
-	for my $col ($col_min .. $col_max) {
-		my $cell = $worksheet->get_cell( $rowi, $col);
-		next unless $cell;
-		if ($cell->value() eq "WG12 nb"){
-			$WGnb_col = $col;
-		}
-		elsif ($cell->value() eq "Edition"){
-			$ed_col = $col;
-		}
-		elsif ($cell->value() eq "Element type"){
-			$type_col = $col;
-		}
-		elsif ($cell->value() eq "wg updated in stepmod?"){
-			$modified_col = $col;
-			#print "$modified_col\n";
-		}
-		elsif ($cell->value() eq "Module name"){
-			$name_col = $col;
-		}
-	}
-#Saving of non modified modules 
-	for my $row ($row_min .. $row_max) {
-		my $cell = $worksheet->get_cell( $row, $modified_col);
-		my $value = $cell->value();
-		if ($row != 0 && $value ne 'y') {
-			my $WGnb_cell = $worksheet->get_cell( $row, $WGnb_col);
-			my $ednb_cell = $worksheet->get_cell( $row, $ed_col);
-			my $name_cell = $worksheet->get_cell( $row, $name_col);
-			my $WGnb = $WGnb_cell->value();
-			my $ednb = $ednb_cell->value();
-			my $name = $name_cell->value();
-			my $type_cell = $worksheet->get_cell( $row, $type_col);
-			if (defined $name && $name ne ""){
-				$name = lc($name);
-				chomp $name;
-				$count = $count +1;
-				if ($type_cell->value() eq 'ARM EXPRESS') {
-					updatearm($WGnb, $name);
-				}
-				if ($type_cell->value() eq 'MIM EXPRESS') {
-					updatemim($WGnb, $name);
-				}
-				if ($type_cell->value() eq 'Document') {
+    my ($row_min, $row_max) = $worksheet->row_range();
+    for my $row ($row_min .. $row_max) {
+        my $info_cell = $worksheet->get_cell( $row, $info_col);
+        my $info = $info_cell->value();
+####### Get the name of the document to modify
+        my $ed = index($info,'ed');
+####### to only get modules (4 numbers after ISO 10303-xxxx)
+        if ($ed == 15){ 
+        	my $wgnb_cell = $worksheet->get_cell( $row, $WGnb_col);
+        	$table[0] = $wgnb_cell->value();
+        	my $nb1 = $ed +4; #ed + the number + the spaces
+########### Action for each type of document
+        	$type[0] = 'Document';
+        	$type[1] = 'ARM EXPRESS';
+        	$type[2] = 'MIM EXPRESS';
+        	for my $elt (@type){
+        		if ($info =~ m/$elt/){
+        			my $nb2 = index($info, $elt) -1;
+        			my $length = $nb2-$nb1;	
+        			$table[1] = substr($info,$nb1,$length);
+        			chomp $table[1];
+        			$table[2] = $elt;
+        		}
+        	}
+        	if ($table[2] ne 'Document') {
+				update($table[0], $table[1], $table[2]);
+		 	}	
+		 	if ($table[2] eq 'Document') {
 					my $WGnbarm;
 					my $WGnbmim;
 					for my $otherrow ($row_min .. $row_max) {
-						my $same_name_cell = $worksheet->get_cell( $otherrow, $name_col);
-						my $othertype = $worksheet->get_cell( $otherrow, $type_col);
-						if  ($same_name_cell->value() eq $name && $othertype->value() eq 'ARM EXPRESS') {
-							my $WGnbarm_cell =  $worksheet->get_cell( $otherrow, $WGnb_col);
-							$WGnbarm = $WGnbarm_cell->value();
-						}
-						elsif ($same_name_cell->value() eq $name && $othertype->value() eq 'MIM EXPRESS') {
-							my $WGnbmim_cell =  $worksheet->get_cell( $otherrow, $WGnb_col);
-							$WGnbmim = $WGnbmim_cell->value();
+						my $cell = $worksheet->get_cell( $otherrow, $info_col);
+						my $info1 = $cell->value();
+						my $ed1 = index($info1,'ed');
+						if ($ed1 == 15){ 
+							my $nb3 = $ed1 +4;
+							if ($info1 =~ m/$type[1]/){
+								my $nb4 = index($info1, $type[1]) -1;
+								my $length1 = $nb4-$nb3;
+								my $name = substr($info1,$nb3,$length1);
+								chomp $name;
+								if ($name eq $table[1]){
+									my $arm = $worksheet->get_cell( $otherrow, $WGnb_col);
+									$WGnbarm = $arm->value();
+								}	
+							}
+							if ($info1 =~ m/$type[2]/){
+								my $nb4 = index($info1, $type[2]) -1;
+								my $length1 = $nb4-$nb3;
+								my $name = substr($info1,$nb3,$length1);
+								chomp $name;
+								if ($name eq $table[1]){
+									my $mim = $worksheet->get_cell( $otherrow, $WGnb_col);
+									$WGnbmim = $mim->value();
+								}
+							} 	
 						}
 					}
-					#print "$WGnbarm et $WGnbmim\n";
-					updatemodule($WGnb, $name, $ednb, $WGnbarm, $WGnbmim);
-				}
-			}	
-		}
-	}
+					updatemodule($table[0], $table[1], $WGnbarm, $WGnbmim);
+		  	}
+         }
+     }
 }
-print "$count modules updated\n";
 
 ###############################################################
-# ARM's modification
+# ARM or MIM modification
 ###############################################################
-sub updatearm {
-	my ($WGnb, $name) = @_;
-	my $Nnb;
-	my $filename = "/Users/klt/Projects/EclipseWS/stepmod/data/modules/$name/arm.exp";
-	my $newfile= "/Users/klt/Projects/EclipseWS/stepmod/data/modules/$name/temp.txt";
-#	my $filename = "/Users/aminata/Documents/ISO/WG12/STEP-ISO10303/stepmod/assembly_component/arm.exp";
+###### for tests ######
+# gray up filenames, newfiles, open fh2 , print fh2, close fh2, unlink filename, move
+sub update {
+	my ($WGnb, $name, $type) = @_;
+	my $filename;
+	if ($type eq 'ARM EXPRESS'){
+		 $filename = "../data/modules/$name/arm.exp"; 
+		#$filename = "/Users/aminatambengue/Documents/ISO/WG12/STEP-ISO10303/stepmod/assembly_component/arm.exp";
+	}
+	if ($type eq 'MIM EXPRESS'){
+		 $filename = "../data/modules/$name/mim.exp";		
+		#$filename = "/Users/aminatambengue/Documents/ISO/WG12/STEP-ISO10303/stepmod/assembly_component/mim.exp";
+	}	
+	my $newfile= "../data/modules/$name/temp.txt";
 	my ($fh,$fh2);
 	open ($fh,'<', $filename) or die "Impossible d'ouvrir le fichier $filename en lecture\n";
 	open ($fh2, '>>', $newfile) or die "Impossible d'ouvrir le fichier $newfile en écriture \n";
 	my $line;
 	while (defined ($line = <$fh>)){
-		my $char = 'ISO TC184/SC4/WG12 ';
+		my $char = 'N';
 		my $result = index($line,$char);
-		my $char1 = 'Supersedes ISO TC184/SC4/WG12 ';
-		my $result1 = index($line, $char1);
-		if ($result != -1 && $line=~ m/-/) { #groups the 2 conditions in one 
-			$result = $result +19;
-			$Nnb= substr ($line, $result, 5);
-			#print $str."\n";
+		my $title = 'ISO TC184/SC4/WG12';
+		if ($line =~ m/$title/ && $line=~ m/-/) { 
+			my $Nnb= substr ($line, $result, 5);
 			$line=~ s/$Nnb/$WGnb/g;
-#			print "$line\n";
+			print "$line\n";
 		}
-		elsif ($result1 != -1) {
-			$result1 = $result1 + 30;
-			my $str1 = substr ($line, $result1, 5);
-			$line =~ s/$str1/$Nnb/g;
-#			print "$line\n";
-		}
-		print $fh2 $line; #deletes then re-write the all document
+####### effacer puis réécrire tout le document		
+		print $fh2 $line; 
 	}
 	close $fh;
 	close $fh2;
 	unlink $filename;
-	#File::Copy module for the naming of files
+### module File::Copy pour le renommage de fichiers
 	move $newfile, $filename;	
-
 }
 
-###############################################################
-# MIM's modification
-###############################################################
-sub updatemim {
-	my ($WGnb, $name) = @_;
-	my $Nnb;
-	my $filename = "/Users/klt/Projects/EclipseWS/stepmod/data/modules/$name/mim.exp";
-	my $newfile= "/Users/klt/Projects/EclipseWS/stepmod/data/modules/$name/temp.txt";
-#	my $filename = "/Users/aminata/Documents/ISO/WG12/STEP-ISO10303/stepmod/assembly_component/mim.exp";
-	my ($fh,$fh2);
-	open ($fh,'<', $filename) or die "Impossible d'ouvrir le fichier $filename en lecture\n";
-	open ($fh2, '>>', $newfile) or die "Impossible d'ouvrir le fichier $newfile en écriture \n";
-	my $line;
-	while (defined ($line = <$fh>)){
-		my $char = 'ISO TC184/SC4/WG12 ';
-		my $result = index($line,$char);
-		my $char1 = 'Supersedes ISO TC184/SC4/WG12 ';
-		my $result1 = index($line, $char1);
-		if ($result != -1 && $line=~ m/-/) { 
-			$result = $result +19;
-			$Nnb= substr ($line, $result, 5);
-			#print $str."\n";
-			$line=~ s/$Nnb/$WGnb/g;
-#			print "$line\n";
-		}
-		elsif ($result1 != -1 && $line=~ m/Supersedes/) {
-			$result1 = $result1 + 30;
-			my $str1 = substr ($line, $result1, 5);
-			$line =~ s/$str1/$Nnb/g;
-#			print "$line\n";
-		}
-		print $fh2 $line; #deletes then re-write the all document
-	}
-	close $fh;
-	close $fh2;
-	unlink $filename;
-	#File::Copy module for the naming of files
-	move $newfile, $filename;
-}
 ###############################################################
 # module.xml modification
 ###############################################################
+###### for tests ######
+# gray up filenames, newfiles, open fh2 , print fh2, close fh2, unlink filename, move
 sub updatemodule {
-	#print "into the function\n";
-	my ($WGnb, $name, $ednb, $WGnbarm, $WGnbmim) = @_;
-	my $filename = "/Users/klt/Projects/EclipseWS/stepmod/data/modules/$name/module.xml";
-	my $newfile= "/Users/klt/Projects/EclipseWS/stepmod/data/modules/$name/temp.txt";
-#	my $filename = "/Users/aminata/Documents/ISO/WG12/STEP-ISO10303/stepmod/assembly_component/module.xml";
-#	my $filename = "/Users/aminata/Documents/ISO/WG12/STEP-ISO10303/stepmod/functional_specification/module.xml";
+	my ($mod, $name, $arm, $mim)=@_;
+	print "Module: $name\n";
+	my $filename = "../data/modules/$name/module.xml";
+	my $newfile= "../data/modules/$name/temp.txt";
+	#my $filename = "/Users/aminatambengue/Documents/ISO/WG12/STEP-ISO10303/stepmod/assembly_component/module.xml";
+	#my $filename = "/Users/aminatambengue/Documents/ISO/WG12/STEP-ISO10303/stepmod/functional_specification/module.xml";
 	my ($fh,$fh2);
 	open ($fh,'<', $filename) or die "Impossible d'ouvrir le fichier $filename en lecture\n";
 	open ($fh2, '>>', $newfile) or die "Impossible d'ouvrir le fichier $newfile en écriture \n";
 	my $line;
-	my $wgnumber;
-	my $wgnumberarm;
-	my $wgnumbermim;
-	my $pubyear;
+	my $pubyear = '2015-05';
+	my $pubdate = '2015-05-15';
 	while (defined ($line = <$fh>)){
-		my $char = 'version="';
-		my $char1 = 'wg.number="';
-		my $char2 = 'wg.number.arm="';
-		my $char3 = 'wg.number.mim="';
-		my $char4 = 'wg.number.supersedes="';
-		my $char5 = 'wg.number.arm.supersedes="';
-		my $char6 = 'wg.number.mim.supersedes="';
-		my $char7 = 'publication.year="';
-		my $char8 = 'publication.date="';
-		my $char9 = 'previous.revision.year="';
-		my $result = index($line, $char);
-		my $result1 = index($line, $char1);
-		my $result2 = index($line, $char2);
-		my $result3 = index($line, $char3);
-		my $result4 = index($line, $char4);
-		my $result5 = index($line, $char5);
-		my $result6 = index($line, $char6);
-		my $result7 = index($line, $char7);
-		my $result8 = index($line, $char8);
-		my $result9 = index($line, $char9);
-		if ($result != -1 && $line=~ m/version="/) {
-			unless ($line=~m/change version/ || $line=~m/xml version/){
-				my $offset = $result + 10;
+####### replace the wg number
+		my @wg;
+		$wg[0] = 'wg.number="';
+		$wg[1] = 'wg.number.arm="';
+		$wg[2] = 'wg.number.mim="';
+		$wg[3] = 'publication.year="';
+		$wg[4] = 'publication.date="';
+		for my $elt(@wg){
+			if ($line =~ m/$elt/) { 
+				my $result = index($line, $elt);
+				my $lg = length($elt);
+				my $offset = $result + $lg;
 				my $limit = index($line, '"', $offset);
-				my $length = $limit - $result;
-				my $str = substr($line, $result, $length);
-#				print "$str\n";
-				$line =~ s/$str/version="$ednb/g;
-#				print $line; 
-			}
+				my $length = $limit - $offset;
+				if (defined $mod && $elt eq $wg[0]){
+					my $old = substr($line, $offset, $length);
+					my $Nindex= index($mod, 'N') +1;
+					my $wgnb= substr($mod, $Nindex, 4);
+					$line =~ s/$old/$wgnb/g;
+				}
+				if (defined $arm && $elt eq $wg[1]){
+					my $old = substr($line, $offset, $length);
+					my $Nindex= index($arm, 'N') +1;
+					my $wgnb= substr($arm, $Nindex, 4);
+					$line =~ s/$old/$wgnb/g;
+				}	
+				if (defined $mim && $elt eq $wg[2]){
+					my $old = substr($line, $offset, $length);
+					my $Nindex= index($mim, 'N') +1;
+					my $wgnb= substr($mim, $Nindex, 4);
+					$line =~ s/$old/$wgnb/g;
+				}	
+				if ($elt eq $wg[3]){
+					my $old = substr($line, $offset, $length);
+					$line =~ s/$old/$pubyear/g;
+				}
+				if ($elt eq $wg[4]){
+					my $old = substr($line, $offset, $length);
+					$line =~ s/$old/$pubdate/g;
+				}
+				print "$line\n";	
+			}	
 		}
-		if ($result1 != -1 && $line=~ m/wg.number="/) {
-			my $offset = $result1 + 12;
-			my $limit = index($line, '"', $offset);
-			my $length = $limit - $result1;
-			$wgnumber = substr($line, $result1, $length);
-#			print "$wgnumber\n";
-			my $Nindex= index($WGnb, 'N') +1;
-			my $WGnb1= substr($WGnb, $Nindex, 4);
-			$line =~ s/$wgnumber/wg.number="$WGnb1/g;
-#			print $line;
-		}
-		if ($result2 != -1 && $line=~ m/wg.number.arm="/) {
-			my $offset = $result2 + 16;
-			my $limit = index($line, '"', $offset);
-			my $length = $limit - $result2;
-			if (defined $WGnbarm){
-				$wgnumberarm = substr($line, $result2, $length);
-				my $Nindex= index($WGnbarm, 'N') +1;
-				my $WGnbarm1= substr($WGnbarm, $Nindex, 4);
-				$line =~ s/$wgnumberarm/wg.number.arm="$WGnbarm1/g;
-			}
-#			print $line;
-		}
-		if ($result3 != -1 && $line=~ m/wg.number.mim="/) {
-			my $offset = $result3 + 16;
-			my $limit = index($line, '"', $offset);
-			my $length = $limit - $result3;
-			if (defined $WGnbmim){
-				$wgnumbermim = substr($line, $result3, $length);
-				my $Nindex= index($WGnbmim, 'N') +1;
-				my $WGnbmim1= substr($WGnbmim, $Nindex, 4);
-				$line =~ s/$wgnumbermim/wg.number.mim="$WGnbmim1/g;
-			}
-#			print $line;
-		}
-		if ($result4 != -1 && $line=~ m/wg.number.supersedes="/) {
-			my $offset = $result4 + 23;
-			my $limit = index($line, '"', $offset);
-			my $length = $limit - $result4;
-			my $str = substr($line, $result4, $length);
-#			print "$wgnumber\n";
-			if (defined $wgnumber){
-				my $tronc = substr($wgnumber, 11, 4);
-#				print "$tronc\n";
-				$line =~ s/$str/wg.number.supersedes="$tronc/g;
-			}
-#			print $line;
-		}
-		if ($result5 != -1 && $line=~ m/wg.number.arm.supersedes="/) {
-			my $offset = $result5 + 27;
-			my $limit = index($line, '"', $offset);
-			my $length = $limit - $result5;
-			my $str = substr($line, $result5, $length);
-			if (defined $wgnumberarm){
-				my $tronc = substr($wgnumberarm, 15, 4);
-#				print "$tronc\n";
-				$line =~ s/$str/wg.number.arm.supersedes="$tronc/g;
-			}
-#			print $line;
-		}
-		if ($result6 != -1 && $line=~ m/wg.number.mim.supersedes="/) {
-			my $offset = $result6 + 27;
-			my $limit = index($line, '"', $offset);
-			my $length = $limit - $result6;
-			my $str = substr($line, $result6, $length);
-			if (defined $wgnumbermim){
-				my $tronc = substr($wgnumbermim, 15, 4);
-#				print "$tronc\n";
-				$line =~ s/$str/wg.number.mim.supersedes="$tronc/g;
-			}
-#			print $line;
-		}
-		if ($result7 != -1 && $line=~ m/publication.year="/) {
-			my $offset = $result7 +19;
-			my $limit = index($line, '"', $offset);
-			my $length = $limit - $result7;
-			$pubyear = substr($line, $result7, $length);
-			$line =~ s/$pubyear/publication.year="2015-12/g;
-#			print $line;
-		}
-#		print "$pubyear\n";
-		if ($result8 != -1 && $line=~ m/publication.date="/) {
-			my $offset = $result8 + 19;
-			my $limit = index($line, '"', $offset);
-			my $length = $limit - $result8;
-			my $str = substr($line, $result8, $length);
-			$line =~ s/$str/publication.date="2015-12-15/g;
-#			print $line;
-		}
-		if ($result9 != -1 && $line=~ m/previous.revision.year="/) {
-			my $offset = $result9 + 24;
-			my $limit = index($line, '"', $offset); 
-			my $length = $limit - $result9;
-			my $str = substr($line, $result9, $length);
-			if (defined $pubyear){
-				my $tronc = substr($pubyear, 18, 7);
-#				print "$tronc\n";
-				$line =~ s/$str/previous.revision.year="$tronc/g;
-			}
-#			print $line;
-		}
+####### delete then re-write the content of document 		
 		print $fh2 $line;
 	}
-	close $fh;
+### Close the files
+	close $fh; 	
 	close $fh2;
 	unlink $filename;
 	# File::Copy module for the naming of files

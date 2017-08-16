f38a8e7e659594a87320368bbb96e7d1668ded62
kevletu <kevletu>
2017-08-16 19:38:12 +0000

    corrected path errors

--- utils/part1000/cvs_update_tagged_parts_of_a_cr.sh
+++ utils/part1000/cvs_update_tagged_parts_of_a_cr.sh
@@ -20,10 +20,12 @@ echo "Error: first, second or both arguments missing. "
 exit
 fi
 
-java -jar $1/stepmod/etc/saxon6-5-5/saxon.jar $1/stepmod/publication/part1000/$2/publication_index.xml $1/stepmod/utils/part1000/pub_index_to_parts_list_for_cvs_update.xsl | xargs cvs update -RAdr $2 | tee $1/$2_cvs_update_log.txt
+cd $1
+
+java -jar stepmod/etc/saxon6-5-5/saxon.jar stepmod/publication/part1000/$2/publication_index.xml stepmod/utils/part1000/pub_index_to_parts_list_for_cvs_update.xsl | xargs cvs update -RAdr $2 | tee $1/$2_cvs_update_log.txt
 
 #tee creates log in workspace
 
-cvs update -RAr $2 -CdP $1/stepmod/data/basic | tee $1/$2_data_basic_cvs_update_log.txt
+cvs update -RAr $2 -CdP stepmod/data/basic | tee $1/$2_data_basic_cvs_update_log.txt
 
 exit

039701ad21a86d4f0f092ee5920114d9afbd88dd
kevletu <kevletu>
2017-08-24 16:53:42 +0000

    updated using express engine

--- utils/part1000/concatenate_arm_and_mim_schema.sh
+++ utils/part1000/concatenate_arm_and_mim_schema.sh
@@ -1,43 +1,48 @@
 #!/bin/bash
-
-### or /bin/sh?
-#
 #
 # WARNING : WORKING DRAFT - DEVLOPMENT IN PROGRESS
 #
-#
-##### Preconditions:
-# needs concatenate_schema.jar, concatenate_mim.prop, concatenate_arm.prop and in the same directory
+# PURPOSE: takes one short form schema and a stepmod directory and creates a concatenated file of eash called short form schemas.
+# LOGS: logs are produced in the workspace.
+# PRECONDITION: Express Engine 4.3.3 or later is installed and linked to the command line name "eengine"
 
 
-# arg 1: workspace absolute path
-# NOT YET IMPLETMENTED arg 2: part number of the AP module, e.g. 410
-# Hard corded for 410
-
-if [ -z $1 ];
+if [ -z $1 ] || [ -z $2 ];
 then
-echo "Error: workspace absolute path argument missing. "
+    echo "Error: one or two arguments are missing. Argument 1 is workspace's absolute path argument and argument 2 is a name of a module. Note: path should not end by the character '/'."
 exit
 fi
 
-echo "stepmod=$1/stepmod" > concatenate_arm.prop
-echo "type=arm" >> concatenate_arm.prop
-echo "roots=ap210_electronic_assembly_interconnect_and_packaging_design" >> concatenate_arm.prop
+eengine --concat_schema -schema $1/stepmod/data/modules/$2/arm.exp -stepmod $1/stepmod/ -mode arm_shortform | tee $1/$2_arm_concat_ee_log.txt
 
-echo "stepmod=$1/stepmod" > concatenate_mim.prop
-echo "type=mim" >> concatenate_mim.prop
-echo "roots=ap210_electronic_assembly_interconnect_and_packaging_design" >> concatenate_mim.prop
+eengine --concat_schema -schema $1/stepmod/data/modules/$2/mim.exp -stepmod $1/stepmod/ -mode mim_shortform | tee $1/$2_mim_concat_ee_log.txt
 
-more concatenate_arm.prop
-more concatenate_mim.prop
 
-java -cp concatenate_schema.jar modules.LongForm concatenated_mim.exp concatenate_mim.prop >outmim 2>outerrmim
-java -cp concatenate_schema.jar modules.LongForm concatenated_arm.exp concatenate_arm.prop >outarm 2>outerrarm
 
-more outmim
-more outarm
-more outerrmim
-more outerrarm
+
+###########################################################################
+##### Preconditions:
+# needs concatenate_schema.jar, concatenate_mim.prop, concatenate_arm.prop and in the same directory
+## commands using the old java scripts from stepmod
+#echo "stepmod=$1/stepmod" > concatenate_arm.prop
+#echo "type=arm" >> concatenate_arm.prop
+#echo "roots=ap210_electronic_assembly_interconnect_and_packaging_design" >> concatenate_arm.prop
+
+#echo "stepmod=$1/stepmod" > concatenate_mim.prop
+#echo "type=mim" >> concatenate_mim.prop
+#echo "roots=ap210_electronic_assembly_interconnect_and_packaging_design" >> concatenate_mim.prop
+
+#more concatenate_arm.prop
+#more concatenate_mim.prop
+
+#java -cp concatenate_schema.jar modules.LongForm concatenated_mim.exp concatenate_mim.prop >outmim 2>outerrmim
+#java -cp concatenate_schema.jar modules.LongForm concatenated_arm.exp concatenate_arm.prop >outarm 2>outerrarm
+
+#more outmim
+#more outarm
+#more outerrmim
+#more outerrarm
+###########################################################################
 
 
 exit

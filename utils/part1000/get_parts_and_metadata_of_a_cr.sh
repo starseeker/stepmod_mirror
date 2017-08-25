83e64a756a79e5ef24e60356007d558310dbb1ed
kevletu <kevletu>
2017-08-25 16:22:21 +0000

    first version

---
+++ utils/part1000/get_parts_and_metadata_of_a_cr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#
+# WARNING - UNDER DEVLOPMENT
+#
+#PURPOSE: Get a list of parts from a CR with N numbers, CR id, edition number, etc. - it uses pub-index_to_wg-n-tables.xsl function 4
+
+
+if [ -z $1 ] || [ -z $2 ];
+then
+echo "Error: first, second or both arguments missing. Two arguments to be specified: 1) absolute path of workspace where stepmod is checked out (e.g.: /Users/klt/Projets/workspace) ---- 2) name of the CR (e.g.: CR_PDM_1)."
+exit
+fi
+
+cd $1
+
+java -jar stepmod/etc/saxon6-5-5/saxon.jar stepmod/publication/part1000/$2/publication_index.xml stepmod/utils/pub-index_to_wg-n-tables.xsl
+
+
+exit

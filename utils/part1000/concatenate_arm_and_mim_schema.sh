0733b14e3d126d784021ed29e972adbac5fb9aaa
kevletu <kevletu>
2017-09-03 18:33:52 +0000

    considered new arg 1 path, removed piped tee, added exec logging at the beginning

--- utils/part1000/concatenate_arm_and_mim_schema.sh
+++ utils/part1000/concatenate_arm_and_mim_schema.sh
@@ -4,18 +4,29 @@
 #
 # PURPOSE: takes one short form schema and a stepmod directory and creates a concatenated file of eash called short form schemas.
 # LOGS: logs are produced in the workspace.
-# PRECONDITION: Express Engine 4.3.3 or later is installed and linked to the command line name "eengine"
+# PRECONDITION: Express Engine 4.3.2 or later is installed and linked to the command line name "eengine"
 
+
+exec > >(tee -i $1/../$2_concat_log.txt)
+exec 2>&1
 
 if [ -z $1 ] || [ -z $2 ];
 then
-    echo "Error: one or two arguments are missing. Argument 1 is workspace's absolute path argument and argument 2 is a name of a module. Note: path should not end by the character '/'."
+	echo "Error: first, second or both arguments missing. Two arguments to be specified: 1) absolute path ending in 'stepmod', with no trailing slash, of checked-out stepmod repository (e.g.: /Users/klt/Projets/workspace/stepmod); 2) name a module (e.g.: ap210_electronic_assembly_interconnect_and_packaging_design )."
 exit
 fi
 
-eengine --concat_schema -schema $1/stepmod/data/modules/$2/arm.exp -stepmod $1/stepmod/ -mode arm_shortform | tee $1/stepmod/data/modules/$2/arm_concat_ee_log.txt
+eengine --concat_schema -schema $1/data/modules/$2/arm.exp -stepmod $1 -mode arm_shortform
+eengine --concat_schema -schema $1/data/modules/$2/mim.exp -stepmod $1 -mode mim_shortform
 
-eengine --concat_schema -schema $1/stepmod/data/modules/$2/mim.exp -stepmod $1/stepmod/ -mode mim_shortform | tee $1/stepmod/data/modules/$2/mim_concat_ee_log.txt
+
+# prompt the user if he wants to generate concat files from STEPmod, SMRL, or Both
+
+exit
+
+
+
+
 
 
 
@@ -27,22 +38,19 @@ eengine --concat_schema -schema $1/stepmod/data/modules/$2/mim.exp -stepmod $1/s
 #echo "stepmod=$1/stepmod" > concatenate_arm.prop
 #echo "type=arm" >> concatenate_arm.prop
 #echo "roots=ap210_electronic_assembly_interconnect_and_packaging_design" >> concatenate_arm.prop
-
+#
 #echo "stepmod=$1/stepmod" > concatenate_mim.prop
 #echo "type=mim" >> concatenate_mim.prop
 #echo "roots=ap210_electronic_assembly_interconnect_and_packaging_design" >> concatenate_mim.prop
-
+#
 #more concatenate_arm.prop
 #more concatenate_mim.prop
-
+#
 #java -cp concatenate_schema.jar modules.LongForm concatenated_mim.exp concatenate_mim.prop >outmim 2>outerrmim
 #java -cp concatenate_schema.jar modules.LongForm concatenated_arm.exp concatenate_arm.prop >outarm 2>outerrarm
-
+#
 #more outmim
 #more outarm
 #more outerrmim
 #more outerrarm
 ###########################################################################
-
-
-exit

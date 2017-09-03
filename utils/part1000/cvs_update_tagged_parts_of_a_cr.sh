727d93c833c887e25a61dbb133c7091f8cb72f91
kevletu <kevletu>
2017-09-03 18:28:27 +0000

    removed piped tee, added exec logging at the beginning

--- utils/part1000/cvs_update_tagged_parts_of_a_cr.sh
+++ utils/part1000/cvs_update_tagged_parts_of_a_cr.sh
@@ -13,11 +13,12 @@
 # WARNING - UNDER DEVLOPMENT
 # WARNING - $1 must end in 'stepmod', with no trailing slash.
 
-
+exec > >(tee -i $1/../$2_cvs_update_log.txt)
+exec 2>&1
 
 if [ -z $1 ] || [ -z $2 ];
 then
-echo "Error: first, second or both arguments missing. Two arguments to be specified: 1) absolute path of workspace where stepmod is checked out (e.g.: /Users/klt/Projets/workspace/stepmod); 2) name of the CR (e.g.: CR_PDM_1)."
+	echo "Error: first, second or both arguments missing. Two arguments to be specified: 1) absolute path ending in 'stepmod', with no trailing slash, of checked-out stepmod repository (e.g.: /Users/klt/Projets/workspace/stepmod); 2) name of the CR (e.g.: CR_PDM_1)."
 exit
 fi
 
@@ -28,8 +29,8 @@ java -jar etc/saxon6-5-5/saxon.jar publication/part1000/$2/publication_index.xml
 
 #tee creates log in workspace
 
-cvs -q -z 3 update -A data/basic | tee publication/part1000/$2/cvs_update_log.txt
+cvs -q -z 3 update -A data/basic
 
-cvs -q -z 3 update -dPr $2 data/basic | tee publication/part1000/$2/cvs_update_log.txt
+cvs -q -z 3 update -dPr $2 data/basic
 
 exit
